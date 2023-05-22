5b744836632b5b07a0b9e3aa894eadabaab71e62
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2023-05-22 10:42:23 +0300

    gamemanager and line script

---
+++ DrawAndPlace/Assets/Script/DrawLine.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+    [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private List<Vector2> fingerPosList;
+     //public Socket _socket;
+
+     Camera _camera;
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+   
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            CreateLine();
+        }
+
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 fingerPos = _camera.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Vector2.Distance(fingerPos, fingerPosList[^1]) >.1f)
+            {
+                UpdateTheLine(fingerPos);
+            }
+        }
+
+    }
+
+    void CreateLine()
+    {
+        fingerPosList.Add(_camera.ScreenToWorldPoint(Input.mousePosition));
+        fingerPosList.Add(_camera.ScreenToWorldPoint(Input.mousePosition));
+        _lineRenderer.SetPosition(0, fingerPosList[0]);
+        _lineRenderer.SetPosition(1, fingerPosList[1]);
+
+
+    }
+
+    void UpdateTheLine(Vector2 incomingFingerPos)
+    {
+        fingerPosList.Add(incomingFingerPos);
+        _lineRenderer.positionCount++;
+        _lineRenderer.SetPosition(_lineRenderer.positionCount-1,incomingFingerPos);
+    }
+}
